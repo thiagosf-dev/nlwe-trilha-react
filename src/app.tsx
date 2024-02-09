f6a6efb313ac9aab724d3eb237e120dfeb5833b9
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2024-02-09 10:48:09 -0300

    FEAT: Creating the NoteCard component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,27 @@
+import logo from './assets/logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 import "./index.css"
 
 export function App() {
   return (
-    <h1 className="text-2xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className={"mx-auto max-w-6xl my-12 space-y-6"}>
+      <img src={logo} alt={"Logo da aplicação"} />
+
+      <form>
+        <input
+          className={"w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"}
+          type="text"
+          placeholder={"Busque em suas notas"}
+        />
+      </form>
+
+      <div className={"h-px bg-slate-700"} />
+
+      <div className={"grid grid-cols-3 gap-6 auto-rows-[250px]"}>
+        <NewNoteCard />
+        <NoteCard note={{ content: "Card 1", date: new Date() }} />
+      </div>
+    </div>
   )
 }
---
+++ src/components/note-card.tsx
@@ -0,0 +1,25 @@
+interface NoteCradProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCradProps) {
+  return (
+    <button
+      className={`rounded-md text-left bg-slate-800 p-5 flex flex-col gap-3 overflow-hidden outline-none relative hover:ring-2
+      hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400`}
+    >
+      <span className={"text-sm font-medium text-slate-300"}>{note.date.toISOString()}</span>
+
+      <p className={"text-sm leading-6 text-slate-400"}>
+        {note.content}
+      </p>
+
+      <div
+        className={"absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"}
+      />
+    </button>
+  )
+}
